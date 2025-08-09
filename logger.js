000c501975b270610caae06c3a7b41a905dfe25d
Reed-Solomon-9 <rsolomon45@gmail.com>
2025-08-09 00:32:19 -0700

    updated json with new logger package, added key for Better Stack

--- logger.js
+++ logger.js
@@ -1,12 +1,19 @@
 const winston = require('winston');
+const { Logtail } = require('@logtail/winston');
+
+const logtailTransport = new Logtail('ENVF9A1YDz65qYMHa3kexfcT')
 
 const logger = winston.createLogger({
-  level: 'info', // Set the default log level
-  format: winston.format.json(), // Use JSON format for better parsing
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
   transports: [
     new winston.transports.Console({
-      format: winston.format.simple(), // Use a simple format for the console
+      format: winston.format.simple(),
     }),
+    logtailTransport // Add the Better Stack transport here
   ],
 });
 
